695b0933f6d69d857eb6594e13c8c07cc3f3f7ab
Maksym Shokin <problaze7@gmail.com>
2023-04-08 19:39:28 +0300

    intercepting-requests

--- cypress/e2e/newsletter.cy.js
+++ cypress/e2e/newsletter.cy.js
@@ -5,6 +5,13 @@ describe('Newsletter', () => {
     cy.task('seedDatabase');
   });
   it('should subscribe to newsletter', () => {
+    // intercept http request and return dummy data
+    cy.intercept('POST', '/newsletter*', { status: 201 });
+
+    // wait for intercepted request
+    // cy.intercept('POST', '/newsletter*', { status: 201 }).as('subscribe');
+    // cy.wait('@subscribe')
+
     cy.visit('/');
     cy.get('[data-cy="newsletter-email"]').type('test@example.com');
     cy.get('[data-cy="newsletter-submit"]').click();
