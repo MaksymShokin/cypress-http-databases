4f0bc22c285553b983943646d9419d7178c19b7c
Maksym Shokin <problaze7@gmail.com>
2023-04-09 11:55:48 +0300

    more-intercepting

--- cypress/e2e/newsletter.cy.js
+++ cypress/e2e/newsletter.cy.js
@@ -17,4 +17,12 @@ describe('Newsletter', () => {
     cy.get('[data-cy="newsletter-submit"]').click();
     cy.contains('Thanks for signing up!');
   });
+
+  it('should show error message when user already subscribed', () => {
+    cy.intercept('POST', '/newsletter*', { message: 'Email already exists' });
+    cy.visit('/');
+    cy.get('[data-cy="newsletter-email"]').type('test@example.com');
+    cy.get('[data-cy="newsletter-submit"]').click();
+    cy.contains('Email already exists');
+  });
 });
